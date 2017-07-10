333d8f0751394debeefc9837cc81c89b93838ff6
Sebastien F4GRX <f4grx@f4grx.net>
2017-07-10 17:31:10 +0200

    prototype for detection tool

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hltools
 Hermes-Lite SDR tools
+
+hl-detect.py: attempt to detect hpsdr devices on the local network. Send a probe and display all responses. no formatting yet.
+
---
+++ metis-detect.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#this program discovers all hermes/hermes lite boards on the local net
+#https://wiki.python.org/moin/UdpCommunication
+#https://docs.python.org/2/library/socket.html
+
+#attemt to discover Hermes / Openhpsdr boards using protocol v1
+
+import socket
+
+BCAST="255.255.255.255"
+DEST_PORT=1024
+DISC="\xEF\xFE\x02" + (60 * "\x00")
+
+print "init socket"
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1);
+sock.bind(('',0))
+
+print "send discovery"
+sock.sendto(DISC, (BCAST, DEST_PORT))
+
+sock.settimeout(1)
+while True:
+  try:
+    data,addr = sock.recvfrom(1024)
+    print "from",addr,":",data
+  except Exception as e:
+    print "error ->",e
+    break
+
+print "done"
+sock.close()
+
