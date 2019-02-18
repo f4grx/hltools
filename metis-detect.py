3cefa99a51cb4d2bc4cd0e23d08c56ebc87b705d
Sebastien F4GRX <f4grx@f4grx.net>
2019-02-18 15:09:16 +0100

    updates for python 3

--- metis-detect.py
+++ metis-detect.py
@@ -10,14 +10,14 @@ import socket,metis
 
 BCAST="255.255.255.255"
 DEST_PORT=1024
-DISC="\xEF\xFE\x02" + (60 * "\x00")
+DISC=bytearray([0xEF, 0xFE, 0x02]) + bytearray(60)
 
-print "init socket"
+print("init socket")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1);
 sock.bind(('',0))
 
-print "send discovery"
+print("send discovery")
 sock.sendto(DISC, (BCAST, DEST_PORT))
 
 sock.settimeout(1)
@@ -26,9 +26,9 @@ while True:
     data,addr = sock.recvfrom(1024)
     metis.decode_hwtopc(data,addr)
   except Exception as e:
-    print "error ->",e
+    print("error ->",e)
     break
 
-print "done"
+print("done")
 sock.close()
 
--- metis.py
+++ metis.py
@@ -3,15 +3,15 @@ boards=['Metis','Hermes','Griffin','?','Angelia','Orion','Hermes_Lite']
 def decode_hwtopc(data,addr):
     l = len(data)
     #check header
-    if ord(data[0]) != 0xEF and ord(data[1]) != 0xFE:
-        print "bad header"
+    if data[0] != 0xEF and data[1] != 0xFE:
+        print("bad header")
         return
 
-    cmd = ord(data[2])
+    cmd = data[2]
 
-    mac="%02X:%02X:%02X:%02X:%02X:%02X" % ( ord(data[3]), ord(data[4]), ord(data[5]), ord(data[6]), ord(data[7]), ord(data[8]))
-    version=ord(data[9])
-    board=ord(data[10])
+    mac="%02X:%02X:%02X:%02X:%02X:%02X" % ( data[3], data[4], data[5], data[6], data[7], data[8])
+    version=data[9]
+    board  =data[10]
     try:
       boardname=boards[board]
     except e:
@@ -24,5 +24,5 @@ def decode_hwtopc(data,addr):
     else:
       status="Unknown"
 
-    print addr[0],"[",mac,"]", boardname, "version", version,"Status:",status
+    print(addr[0],"[",mac,"]", boardname, "version", version,"Status:",status)
 
